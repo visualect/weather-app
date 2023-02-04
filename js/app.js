e5e66a94c9e9271eb8e3ae605155371186e4db14
Yan <visualection@gmail.com>
2023-02-04 19:36:51 +0200

    created basic functionality of search page

--- js/app.js
+++ js/app.js
@@ -1,3 +1,53 @@
-const API = 'bd4f968086b92c6cd351be53e08c8589';
+export const API_KEY = 'bd4f968086b92c6cd351be53e08c8589';
+const root = document.getElementById('root');
+const params = new URLSearchParams(location.search);
+const currentCity = params.get('city');
 
-fetch(`http://api.openweathermap.org/geo/1.0/direct?q=London&limit=5&appid=${API}`);
+export function getCurrentWeather(city) {
+  fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${API_KEY}`)
+    .then(res => res.json())
+    .then(json => json);
+}
+
+// function getWeatherForecast(city) {
+//   fetch(`https://api.openweathermap.org/data/2.5/forecast?q=${city}&appid=${API_KEY}`)
+//     .then(res => res.json())
+//     .then(json => console.log(json));
+// }
+
+function load(src) {
+  if (src.endsWith('.js')) {
+    return import(src);
+  } else {
+    return fetch(src).then(res => res.json());
+  }
+}
+
+export default function renderHome(js) {
+  return import(js).then(jsModule => {
+    const page = jsModule.create();
+    root.innerHTML = '';
+    root.append(page);
+  });
+}
+
+export function renderWeatherPage(js, data) {
+  Promise.all([
+    load(js),
+    load(data),
+  ])
+    .then(([jsModule, loadedData]) => {
+      if (loadedData.cod === '404') throw Error('Incorrect city name or city does not exist');
+      const input = document.querySelector('.home__input')
+      history.pushState(null, null, `${location.pathname}/city=${input.value}`);
+      const page = jsModule.create(loadedData);
+      root.innerHTML = "";
+      root.append(page);
+    })
+    .catch(err => {
+      const errDisplay = document.querySelector('.home__err-display');
+      errDisplay.textContent = err.message;
+    });
+}
+
+renderHome('./home.js');
---
+++ js/weather.js
@@ -0,0 +1,8 @@
+// eslint-disable-next-line no-unused-vars
+export function create(data) {
+  const container = document.createElement('div');
+
+  container.classList.add('weather-container');
+
+  return container;
+}
