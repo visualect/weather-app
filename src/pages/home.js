ac608a0cec02c7c2cbbc88632aa279224550f299
Yan <visualection@gmail.com>
2023-09-03 19:55:41 +0200

    Changed home style

--- src/pages/home.js
+++ src/pages/home.js
@@ -4,10 +4,9 @@ export function create() {
   const container = document.createElement('div');
   const form = document.createElement('form');
   const input = document.createElement('input');
-  const displayError = document.createElement('span');
   const inputWrapper = document.createElement('div');
 
-  input.placeholder = 'City';
+  input.placeholder = 'Enter the name of the city';
 
   form.addEventListener('submit', event => {
     event.preventDefault();
@@ -22,11 +21,9 @@ export function create() {
   form.classList.add('home__form');
   input.classList.add('home__input');
   inputWrapper.classList.add('home__input-wrapper');
-  displayError.classList.add('err-display');
 
   inputWrapper.append(input);
   form.append(inputWrapper);
-  form.append(displayError);
   container.append(form);
 
   return container;
